c4cef850bab09769a8085994ebd004388bf68386
AlxInc <108522772+AlxInc@users.noreply.github.com>
2022-12-07 10:22:53 +1030

    missed a section

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -25,6 +25,15 @@ def main():
                 number=100000,
                 globals=globals())
           )
+    print(timeit.timeit(stmt='expenses.categorize_set_comprehension',
+                        setup='''
+                    from . import Expense
+                    expenses = Expense.Expenses()
+                    expenses.read_expenses('data/spending_data.csv')
+                    ''',
+                        number=100000,
+                        globals=globals())
+          )
 
     fig, ax = plt.subplots()
     labels = ['Necessary', 'Food', 'Unnecessary']
